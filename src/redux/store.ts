53c7806a2f951f332c427b2d38c478a6c1966dee
nexgensoft0 <nexgensoft0@gmail.com>
2025-03-29 12:00:39 +0600

    redux rtk query added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./feature/todoSlice";
+import { baseApi } from "./api/api";
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     todo: todoReducer,
   },
 });
